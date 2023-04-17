512c4eff5344ff0350fe9ff1d2b17a05c109237e
Andrew Zenk <andrew.zenk@particle.io>
2023-04-17 15:28:31 -0500

    Create basic flash device action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,44 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+
+import * as ParticleApi from 'particle-api-js'
+const particle = new ParticleApi()
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const accessToken = core.getInput('access-token')
+    const deviceId = core.getInput('device-id')
+    const firmwarePath = core.getInput('firmware-path')
+    const timeoutMs = parseInt(core.getInput('timeout-ms'), 10)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Flash the device
+    await particle.flashDevice({
+      deviceId,
+      files: {file: firmwarePath},
+      auth: accessToken
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    // Wait for the flash to complete
+    const stream = await particle.getEventStream({
+      deviceId,
+      auth: accessToken,
+      name: 'spark/status'
+    })
+
+    await new Promise<void>((resolve, reject) => {
+      const flashTimeout = setTimeout(
+        () =>
+          reject(new Error('timed out waiting for device to come back online')),
+        timeoutMs
+      )
+
+      stream.on('event', data => {
+        if (data.data === 'online') {
+          stream.cancel()
+          clearTimeout(flashTimeout)
+          resolve()
+        }
+      })
+    })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
